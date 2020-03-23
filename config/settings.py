5dbc9f7badb7e47bf9acafd0c309a5245eb9478f
ssayebee <ssayebee@gmail.com>
2020-03-23 14:39:41 +0900

    tmp add room

--- config/settings.py
+++ config/settings.py
@@ -39,9 +39,13 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = ["django_countries"]
 
-PROJECT_APPS = ["users.apps.UsersConfig"]
+PROJECT_APPS = [
+    "core.apps.CoreConfig",
+    "users.apps.UsersConfig",
+    "rooms.apps.RoomsConfig",
+]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
