57e2154445ed0d2ffa376b5eea8ff17e2d4546d5
ssayebee <ssayebee@gmail.com>
2020-04-04 17:40:06 +0900

    Add Templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+
+def all_rooms(request):
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
