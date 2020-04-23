7fd1b89d74ddaf4cef3e6a667659a4f5484f6936
ssayebee <ssayebee@gmail.com>
2020-04-23 17:13:34 +0900

    Add User Login View
    add Login View and Validating Email
    cf) https://academy.nomadcoders.co/courses/637659/lectures/12297860

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
+from django.views import View
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        form = forms.LoginForm(initial={"email": "user@email.com"})
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        return render(request, "users/login.html", {"form": form})
