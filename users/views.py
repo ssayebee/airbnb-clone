b932cb80416919fe660f15c92d11475a7dc6bb34
ssayebee <ssayebee@gmail.com>
2020-04-27 16:35:12 +0900

    Add Login Logout with FormView

--- users/views.py
+++ users/views.py
@@ -1,15 +1,26 @@
 from django.views import View
-from django.shortcuts import render
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
-        form = forms.LoginForm(initial={"email": "user@email.com"})
-        return render(request, "users/login.html", {"form": form})
+class LoginView(FormView):
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-        return render(request, "users/login.html", {"form": form})
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
