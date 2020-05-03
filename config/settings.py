d2012e7c4f886019a0844dad1bbd55244fe683f8
ssayebee <ssayebee@gmail.com>
2020-05-03 18:31:24 +0900

    Add RoomList page with tailwind

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 AUTH_USER_MODEL = "users.User"
 
 
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      spacing: {
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh"
+      },
+      borderRadius: {
+        xl: "1.5rem"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
