6a927e9f3912588ac2d9617ea876b6d39b3cc240
ssayebee <ssayebee@gmail.com>
2020-05-04 19:14:40 +0900

    Add SignIn, Up page with tailwind

--- users/views.py
+++ users/views.py
@@ -33,11 +33,6 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        "first_name": "Ha",
-        "last_name": "SangYeop",
-        "email": "ssayebee@gmail.com",
-    }
 
     def form_valid(self, form):
         form.save()
