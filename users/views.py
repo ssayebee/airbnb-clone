931ef8c731feab6fe7bb446f23a83d4eba06a20d
ssayebee <ssayebee@gmail.com>
2020-06-26 00:54:35 +0900

    Fix url

--- users/views.py
+++ users/views.py
@@ -73,7 +73,8 @@ def complete_verification(request, key):
 
 def github_login(request):
     client_id = os.environ.get("GH_ID")
-    redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
+    url = os.environ.get("URL", "http://127.0.0.1:8000")
+    redirect_uri = f"{url}/users/login/github/callback"
     return redirect(
         f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user"
     )
@@ -143,7 +144,8 @@ def github_callback(request):
 
 def kakao_login(request):
     client_id = os.environ.get("KAKAO_ID")
-    redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+    url = os.environ.get("URL", "http://127.0.0.1:8000")
+    redirect_uri = f"{url}/users/login/kakao/callback"
     return redirect(
         f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
     )
@@ -157,7 +159,8 @@ def kakao_callback(request):
     try:
         code = request.GET.get("code")
         client_id = os.environ.get("KAKAO_ID")
-        redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+        url = os.environ.get("URL", "http://127.0.0.1:8000")
+        redirect_uri = f"{url}/users/login/kakao/callback"
         token_request = requests.get(
             f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}"
         )
